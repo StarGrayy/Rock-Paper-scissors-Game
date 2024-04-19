96508408be3284a7d1cb661a51447b162fdf3f56
Swati <93772681+StarGrayy@users.noreply.github.com>
2024-04-19 20:12:48 +0530

    uploaded script, style and html files!

---
+++ app.js
@@ -0,0 +1,67 @@
+let userScore =0;
+let compScore =0;
+
+const choices = document.querySelectorAll(".choice");
+const msg=document.querySelector("#msg");
+const userScorePara=document.querySelector("#user-score");
+const compScorePara=document.querySelector("#comp-score");
+
+const genCompChoice=()=>{
+    const options = ["rock","paper","scissors"];
+    const randIdx=Math.floor(Math.random()*3);
+    return options[randIdx];
+}
+
+const drawGame=()=>{
+    msg.innerText="Game was draw. Play Again!";
+    msg.style.backgroundColor="#081b31";
+}
+
+const showWinner=(userWin,userChoice,compChoice)=>{
+    if(userWin){
+        userScore++;
+        userScorePara.innerText=userScore;
+        msg.innerText=`You Win:) Your ${userChoice} beats ${compChoice}`;
+        msg.style.backgroundColor="green";
+    }else{
+        compScore++;
+        compScorePara.innerText=compScore;
+        msg.innerText=`You Lose:( ${compChoice} beats your ${userChoice}`;
+        msg.style.backgroundColor="red";
+    }
+}
+
+const playGame =(userChoice)=>{
+    console.log("userChoice=",userChoice);
+    /*generate compChoice were breaking each work in 
+    function because this is the way of modular 
+    programming where each func has just one work 
+    also known as Single Responsibility Principle*/ 
+    const compChoice=genCompChoice();
+    console.log(`compChoice= ${compChoice}`);
+
+    if(userChoice===compChoice){
+        //Draw Game.
+        drawGame();
+    }else{
+        let userWin=true;
+        if(userChoice==="rock"){
+            userWin=compChoice==="paper"?false:true;
+        }
+        else if(userChoice==="paper"){
+            userWin=compChoice==="scissor"?false:true;
+        }
+        else{
+            userWin=compChoice==="rock"?false:true;
+        }
+        showWinner(userWin,userChoice,compChoice);
+    }
+};
+
+choices.forEach((choice)=>{
+    // console.log(choice);
+    choice.addEventListener("click",()=>{
+        const userChoice= choice.getAttribute("id");
+        playGame(userChoice);
+    });
+});
